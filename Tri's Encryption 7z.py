c47c46a45593f62ba29eac516ca35d597e0712c3
Trifortay <50177779+Trifortay@users.noreply.github.com>
2025-07-14 14:06:50 -0400

    Add files via upload

---
+++ Tri's Encryption 7z.py
@@ -0,0 +1,13 @@
+import hashlib
+
+dictionarysize= {
+    1:"1m", 2:"2m", 3:"4m",
+    4:"8m", 5:"16m", 6:"32m",
+    7:"64m", 8:"128m", 9:"256m",
+    10:"512m", 11:"1024m", 12:"2048m"}
+
+def encrypt():
+    pass
+
+def decrypt():
+    pass
