ea63ad1b685334066b3c2ef53fb81fbe9bbb55e3
Trifortay <50177779+Trifortay@users.noreply.github.com>
2025-07-15 09:22:46 -0400

    Add files via upload

--- Tri's Encryption 7z.py
+++ Tri's Encryption 7z.py
@@ -1,6 +1,8 @@
+import subprocess
 import hashlib
 import random
 import time
+import sys
 import os
 
 pythonpath = os.path.dirname(os.path.abspath(__file__)) # Grabs this python script location
@@ -20,59 +22,88 @@ dsize_display=" 1: 1MB (Very low compression but very fast)\n 2: 2MB\n 3: 4MB\n 
 #print(dsize_display)
 
 def hashing(newkey):
-    newkey=newkey.encode('utf-8')
     start_time=time.perf_counter_ns()
     for x in range(1000000):
         newkey=hashlib.sha3_512(newkey).digest()
         if (x+1) % 50000 == 0:
             print(f"\rSHA 512 Hash {x+1:7}/1000000 finished @ {(x+1)/(time.perf_counter_ns()-start_time)*1e9:9.2f} hashes per second",end="",flush=True)
+    print()        
     
     start_time=time.perf_counter_ns()
     for x in range(1000000):
         newkey=hashlib.sha3_256(newkey).digest()
         if (x+1) % 50000 == 0:
             print(f"\rSHA 256 Hash {x+1:7}/1000000 finished @ {(x+1)/(time.perf_counter_ns()-start_time)*1e9:9.2f} hashes per second.",end="",flush=True)
-    
-    print()
-    return newkey.hex()
+    print()            
+    return newkey
 
-def encrypt(dsize,masterkey):
-    pass
+def encrypt(dsize,masterkey,salt):
+    inputfolder=os.path.join(pythonpath,"input","*")
+    outputfolder=os.path.join(pythonpath,"output","output.tri7z")
+    subprocess.run(["7z","a","-mx=9","-mhe=on","-m0=LZMA2",f"-md={dsize}",f"-p{masterkey}",outputfolder,inputfolder])
+    with open(outputfolder, "ab") as file:
+        file.write(salt)
+    
 
 def decrypt(masterkey):
+    inputfolder=os.path.join(pythonpath,"input","input.tri7z")
+    outputfolder=os.path.join(pythonpath,"output")
+    subprocess.run(["7z","x",f"-p{masterkey}",inputfolder,f"-o{outputfolder}"])
     pass
 
-print("Make sure you want the folder that you wanna Encrypt or Decrypt is in the input folder where this Python script is.\nAlso make sure if you used this before, the folders are empty.")
+print("Make sure you want the folder that you wanna Encrypt or Decrypt\nis in the input folder where this Python script is.\nAlso make sure if you used this before, the folders are empty.")
 time.sleep(1)
 action1="\0"
 enckey="\0"
 masterkey="\0"
 salt=format(random.randint(0,4294967295),'08x')
+salt=bytes.fromhex(salt)
 
 
-while True:
-    while action1 != "e" and action1 != "d":
-        action1=input("Encrypt or Decrypt? (e/d): ").lower()
-    if action1 == "e":
-        print("This will Encrypt everything in the directory [input/].")
-        print("Input Dictionary size for LZMA2.")
-        print(dsize_display)
-        dsize = 0
-        while  dsize < 1 or dsize > 12:
-            try:
-                dsize=int(input("\nDictionary Size (Input 1-12): "))
-            except ValueError:
-                continue
-        
-        enckey=input("Now enter a password: ")
-        newkey="".join([enckey,salt])
-        masterkey=hashing(newkey)
-        print(masterkey)
-        
-        #print(newkey)
-        encrypt(dsize,masterkey)
-        break
-    elif action1 == "d":
-        print("This will Decrypt everything in the directory [output/].")
+
+while action1 != "e" and action1 != "d":
+    action1=input("Encrypt or Decrypt? (e/d): ").lower()
+if action1 == "e":
+    print("This will Encrypt everything in the directory [input/] into the directory [output/].")
+    print("Input Dictionary size for LZMA2.")
+    print(dsize_display)
+    dsize = 0
+    while  dsize < 1 or dsize > 12:
+        try:
+            dsize=int(input("\nDictionary Size (Input 1-12): "))
+        except ValueError:
+            continue
     
+    enckey=input("Enter a password: ")
+    enckey=enckey.encode('utf-8')
+    newkey=b''.join([enckey,salt])
+    masterkey=hashing(newkey)
+    print(masterkey)
+    dsize=dictionarysize[dsize]
+    print(dsize)
+    #esaltbin=bytes.fromhex(salt)
+    encrypt(dsize,masterkey,salt)
+    print(f"Salt: {salt}")
+    print(f"Saltbin: {salt}")
 
+elif action1 == "d":
+    inputfolder=os.path.join(pythonpath,"input","input.tri7z")
+    outputfolder=os.path.join(pythonpath,"output")
+    try:
+        with open(inputfolder,"r+b") as file:
+            file.seek(-4,2)
+            salt=file.read(4)
+            file.seek(-4,2)
+            file.truncate()
+    except FileNotFoundError:
+        print("\nFILE NOT FOUND!")
+        print("Make sure the encrypted file is named [input.tri7z]\n")
+        time.sleep(5)
+        sys.exit()
+    print("This will Decrypt everything in the directory [input/] into the directory [output/].")
+    #salt=saltbin.hex()
+    enckey=input("Enter the password: ")
+    enckey=enckey.encode('utf-8')
+    newkey=b''.join([enckey,salt])
+    masterkey=hashing(newkey)
+    decrypt(masterkey)
